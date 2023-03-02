b40c81971ab61fee5d01e2b0e91bfb640140e48c
Nicholas Masumoto <nmasumot@ucsd.edu>
2023-03-01 20:55:23 -0800

    Added local Storage and github link

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# homework4
+# Nicholas Masumoto A15864028
+
+https://63ffcb040ff4aa0007499bca--keen-alfajores-2de91c.netlify.app/
+
--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
-export {promptListener};
+export {promptListener,loadEntries};
 
 //Blog Prompt Variables
 const promptShow = document.getElementById("add-button");
@@ -9,6 +9,8 @@ const epconfirmButton = document.getElementById('eprompt-confirm');
 
 
 var currentSection;
+var oldTitle;
+var oldDate;
 
 function promptListener(){
     promptShow.addEventListener('click', promptFunction);
@@ -21,6 +23,12 @@ function promptFunction(){
     setTimeout(function delay(){
         promptDia.showModal();
     },50);
+    let title = document.getElementById('bTitle');
+    let date = document.getElementById('bDate');
+    let summary = document.getElementById('bSummary');
+    title.value = '';
+    date.value = '';
+    summary.value = '';
 }
 
 function pconfirm(){
@@ -44,6 +52,8 @@ function pconfirm(){
     deleteBtn.addEventListener('click',()=>{
         let buttonClicked = deleteBtn;
         let parent = buttonClicked.parentNode;
+        let delID = parent.childNodes[0].innerHTML+parent.childNodes[1].innerHTML;
+        deleteLC(delID);
         while(parent.firstChild != null){
             parent.firstChild.remove();
         }
@@ -56,13 +66,15 @@ function pconfirm(){
         let eTitle = parent.childNodes[0].innerHTML;
         let eDate = parent.childNodes[1].innerHTML;
         let eSummary = parent.childNodes[2].innerHTML;
-        epromptDia.showModal();
         let title = document.getElementById('eTitle');
         let date = document.getElementById('eDate');
         let summary = document.getElementById('eSummary');
         title.value = eTitle;
         date.value = eDate;
         summary.value = eSummary;
+        oldTitle = eTitle;
+        oldDate = eDate;
+        epromptDia.showModal();
     });    
     deleteBtn.innerText = 'Delete';
     editBtn.innerText = 'Edit';
@@ -80,20 +92,106 @@ function epconfirm(){
     currentSection.childNodes[0].innerHTML = title;
     currentSection.childNodes[1].innerHTML = date;
     currentSection.childNodes[2].innerHTML = summary;
+    editLC(currentSection,oldTitle,oldDate);
 }
 
 function saveToLC(element){
     let postList = JSON.parse(localStorage.getItem('items'));
-    console.log(postList);
     if(postList == null){
         postList = {};
     }
+    let id = element.childNodes[0].innerHTML+element.childNodes[1].innerHTML;
     let entryJSON = {
         title: element.childNodes[0].innerHTML,
         date: element.childNodes[1].innerHTML,
         summary: element.childNodes[2].innerHTML,
     }
-    console.log(postList.length);
-    postList[postList.length] = entryJSON;
+    postList[id] = entryJSON;
     localStorage.setItem('items', JSON.stringify(postList));
+}
+
+function loadEntries(){
+    let postList = JSON.parse(localStorage.getItem('items'));
+    let keyList = Object.keys(postList)
+    keyList.forEach(element => {
+        let post = postList[element]
+        let title = post.title;
+        let date = post.date;
+        let summary = post.summary;
+        let newSection = document.createElement('section');
+        let newTitle = document.createElement('h2');
+        newTitle.innerHTML = title;
+        let newDate = document.createElement('p');
+        newDate.innerHTML = date;
+        let newSummary = document.createElement('p');
+        newSummary.innerHTML = summary;
+        newSection.appendChild(newTitle);
+        newSection.appendChild(newDate);
+        newSection.appendChild(newSummary);
+        let deleteBtn = document.createElement('button');
+        let editBtn = document.createElement('button');
+        deleteBtn.addEventListener('click',()=>{
+            let buttonClicked = deleteBtn;
+            let parent = buttonClicked.parentNode;
+            let delID = parent.childNodes[0].innerHTML+parent.childNodes[1].innerHTML;
+            deleteLC(delID);
+            while(parent.firstChild != null){
+                parent.firstChild.remove();
+            }
+            parent.remove();
+    });
+    editBtn.addEventListener('click',()=>{
+        let buttonClicked = editBtn;
+        let parent = buttonClicked.parentNode;
+        currentSection = parent;
+        let eTitle = parent.childNodes[0].innerHTML;
+        let eDate = parent.childNodes[1].innerHTML;
+        let eSummary = parent.childNodes[2].innerHTML;
+        let title = document.getElementById('eTitle');
+        let date = document.getElementById('eDate');
+        let summary = document.getElementById('eSummary');
+        title.value = eTitle;
+        date.value = eDate;
+        summary.value = eSummary;
+        oldTitle = eTitle;
+        oldDate = eDate;
+        epromptDia.showModal();
+    });    
+    deleteBtn.innerText = 'Delete';
+    editBtn.innerText = 'Edit';
+    newSection.appendChild(deleteBtn);
+    newSection.appendChild(editBtn);
+    newSection.appendChild(document.createElement('hr'));
+    document.getElementById('newPost').appendChild(newSection);
+    });
+}
+
+function deleteLC(id){
+    let postList = JSON.parse(localStorage.getItem('items'));
+    let keyList = Object.keys(postList);
+    //let id = elementToDelete.childNodes[0].innerHTML+elementToDelete.childNodes[1].innerHTML;
+    let newPostList = {};
+    keyList.forEach(element => {
+        if(id != element){
+            newPostList[element] = postList[element];
+        }
+    });
+    localStorage.setItem('items', JSON.stringify(newPostList));
+}
+
+function editLC(elementToDelete, oldTitle, oldDate){
+    let postList = JSON.parse(localStorage.getItem('items'));
+    //let keyList = Object.keys(postList);
+    //let id = oldTitle+oldDate;
+    let newid = elementToDelete.childNodes[0].innerHTML+elementToDelete.childNodes[1].innerHTML;
+    let entryJSON = {
+        title: elementToDelete.childNodes[0].innerHTML,
+        date: elementToDelete.childNodes[1].innerHTML,
+        summary: elementToDelete.childNodes[2].innerHTML,
+    }
+    postList[newid] = entryJSON;
+    localStorage.setItem('items',JSON.stringify(postList));
+    if((oldTitle != entryJSON.title) || oldDate != (entryJSON.date)){
+        deleteLC(oldTitle+oldDate);
+    }
 }
